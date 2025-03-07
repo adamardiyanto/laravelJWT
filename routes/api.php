9c704015a0d3a6d0c4285ce5e935b2aeaf7e36e6
adam <adamardiyantow@gmail.com>
2025-03-07 14:43:59 +0700

    Add Laravel Sanctum for API authentication and create personal access tokens table

---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\JwtMiddleware;
+use App\Http\Controllers\DataController;
+use App\Http\Controllers\UserController;
+
+Route::post('register', [App\Http\Controllers\UserController::class, 'register']);
+Route::post('login', [App\Http\Controllers\UserController::class, 'authenticate']);
+Route::get('open', [App\Http\Controllers\DataController::class, 'open']);
+
+Route::group(['middleware' => JwtMiddleware::class], function() {
+    Route::get('user', [App\Http\Controllers\UserController::class, 'getAuthenticatedUser']);
+    Route::get('closed', [App\Http\Controllers\DataController::class, 'closed']);
+});
+
+
+
