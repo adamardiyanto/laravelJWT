4cf3a09f9d9777fb273e080184189b7f6447eb99
adam <adamardiyantow@gmail.com>
2025-03-07 14:47:49 +0700

    Enhance JWT middleware to handle token invalidation and expiration with appropriate responses; adjust JWT TTL and refresh TTL settings

--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -23,14 +23,27 @@ class JwtMiddleware extends BaseMiddleware
             $user = JWTAuth::parseToken()->authenticate();
         } catch (Exception $e) {
             if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException){
-                return response()->json(['status' => 'Token is Invalid']);
+                $status     = 401;
+                $message    = 'This token is invalid. Please Login';
+                return response()->json(compact('status','message'),401);
             }else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException){
-                return response()->json(['status' => 'Token is Expired']);
+                // If the token is expired, then it will be refreshed and added to the headers
+                try
+                {
+                  $refreshed = JWTAuth::refresh(JWTAuth::getToken());
+                  $user = JWTAuth::setToken($refreshed)->toUser();
+                  $request->headers->set('Authorization','Bearer '.$refreshed);
+                }catch (JWTException $e){
+                    return response()->json([
+                        'code'   => 103,
+                        'message' => 'Token cannot be refreshed, please Login again'
+                    ]);
+                }
             }else{
-                return response()->json(['status' => 'Authorization Token not found']);
+                $message = 'Authorization Token not found';
+                return response()->json(compact('message'), 404);
             }
         }
         return $next($request);
     }
 }
-
